1b0faa6ac60a472c1556232d8a666f967f1b1677
ktacarrasco <kta.carrasco@gmail.com>
2020-03-02 00:20:41 +0100

    camara e imagen mostrada

--- app/src/main/java/com/desafios/desafio4/MainActivity.java
+++ app/src/main/java/com/desafios/desafio4/MainActivity.java
@@ -2,15 +2,63 @@ package com.desafios.desafio4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btn;
+    private ImageView img;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Relacionamos con el XML
+        img = (ImageView)this.findViewById(R.id.imagengrande);
+        btn= (Button) this.findViewById(R.id.btn);
+        //Añadimos el Listener Boton
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent para llamar a la Camara
+            dispatchTakePictureIntent();
+
+
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+           img.setImageBitmap(imageBitmap);
+        }
     }
 
 
-}
+
+    }
+
