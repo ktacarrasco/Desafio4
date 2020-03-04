1d581facddf45e2fc4da26a1b3b33bad8c0981f7
ktacarrasco <kta.carrasco@gmail.com>
2020-03-04 01:55:27 +0100

    Desafio Terminado

--- app/src/main/java/com/desafios/desafio4/MainActivity.java
+++ app/src/main/java/com/desafios/desafio4/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn,btn2;
     private ImageView img;
+    private String valida;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
@@ -25,56 +26,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Relacionamos con el XML
-        img = (ImageView)this.findViewById(R.id.imagengrande);
-        btn= (Button) this.findViewById(R.id.btn);
+        img = (ImageView) this.findViewById(R.id.imagengrande);
+        btn = (Button) this.findViewById(R.id.btn);
         //Añadimos el Listener Boton
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Creamos el Intent para llamar a la Camara
-            dispatchTakePictureIntent();
+                dispatchTakePictureIntent();
+                valida="ok";
 
 
             }
         });
 
-        btn2= (Button) this.findViewById(R.id.btn2);
+        btn2 = (Button) this.findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
 
-            if(resultCode==RESULT_OK){
+            @Override
+            public void onClick(View v) {
 
-            intentExample();
+                if (valida == "ok") {
+                    intentExample();
 
-            }
-            else{
-                Toast.makeText(MainActivity.this, "Para avanzar tiene que sacar foto",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Es Obligatorio la fotografía ", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        private void dispatchTakePictureIntent () {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-           img.setImageBitmap(imageBitmap);
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                img.setImageBitmap(imageBitmap);
+
+            }
         }
+
+        private void intentExample () {
+            Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra("BOOLEAN", true);
+            intent.putExtra("String", "http://www.desafiolatam.com/");
+            startActivity(intent);
+
+
+        }
+
     }
 
-    private void intentExample(){
-        Intent intent = new Intent(this, ResultActivity.class);
-        startActivity(intent);
-
-
-
-
-    }
 
--- app/src/main/java/com/desafios/desafio4/ResultActivity.java
+++ app/src/main/java/com/desafios/desafio4/ResultActivity.java
@@ -2,13 +2,58 @@ package com.desafios.desafio4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private Button btnURL,btnShare;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        btnURL= (Button) this.findViewById(R.id.btnAbrirWeb);
+
+                btnURL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String url = "http://www.desafiolatam.com/";
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        Toast.makeText(ResultActivity.this, "Validación OK enviando a DesafioLatam.com  ", Toast.LENGTH_SHORT).show();
+
+
+
+                    }
+                });
+
+                btnShare=(Button) this.findViewById(R.id.btnCompartir);
+
+                btnShare.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        share(v);
+                        Toast.makeText(ResultActivity.this, "Validación OK compartir ", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        }
+
+    public void share(View v){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "¡Hola! te comparto : " );
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+
     }
-}
+    }
+
+
